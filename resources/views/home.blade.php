a79583fcd17555bd218364052b854ca8fde7c16b
FrancisMosoti <francismosoti20@gmail.com>
2025-01-25 00:18:14 +0300

    Add comment form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,7 +34,48 @@
                 </form>
             </div>
         </div>
-        <div><a href="/posts" class="btn btn-secondary">View Posts</a></div>
+        <div class="flex mx-4">
+            <div><a href="/posts" class="btn btn-secondary">View Posts</a></div>
+        <!-- Button trigger modal -->
+            <div class="mt-4">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Add Comment
+                </button>
+            </div>
+        </div>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <form method="POST" action="{{route('add.comment')}}" class="modal-content">
+        @csrf
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Comment on a post</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Post Id</label>
+                <select class="form-select" name="post_id" aria-label="Default select example">
+                    <option selected>Select the post id</option>
+                    @foreach($posts as $post)
+                        <option value="{{ $post->id }}">{{ $post->id }}</option>
+                    @endforeach
+                 
+                  </select>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControl" class="form-label">Comment</label>
+                <input type="text" name="content" class="form-control" id="exampleFormControl" >
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
+    </div>
+  </div>
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,6 +11,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::post('/home', [App\Http\Controllers\PostController::class, 'store'])->name('create.post');
-Route::get('/posts', [App\Http\Controllers\PostController::class, 'index'])->name('show.post');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::post('/home', [PostController::class, 'store'])->name('create.post');
+Route::get('/posts', [PostController::class, 'index'])->name('show.post');
+Route::post('/comments', [CommentController::class, 'store'])->name('add.comment');
